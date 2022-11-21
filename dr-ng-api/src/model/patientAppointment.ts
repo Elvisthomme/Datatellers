13209cda3b093aee9536adf6ebe727d50728d0e2
Elvis THOMME <elvisthomme@gmail.com>
2022-11-21 17:18:02 +0100

    Create patient model

---
+++ dr-ng-api/src/model/patientAppointment.ts
@@ -0,0 +1,81 @@
+import { DataTypes, Model } from "sequelize";
+import db from "../config/database.config";
+
+interface PatientAppointmentAttribute {
+  id: number;
+  uniqueCode: string;
+  patientName: string;
+  patientPhone: string | null | undefined;
+  patientAddress: string | null | undefined;
+  patientCity: string | null | undefined;
+  patientEmail: string;
+  patientAge: number;
+  patientSex: string;
+  isFirstTime: boolean;
+  commentBefore: string;
+  commentAfter: string;
+  appointmentStatus: string;
+}
+
+/**
+ * ORM representation of a patient appointment
+ */
+class PatientAppointmentInstance extends Model<PatientAppointmentAttribute> {}
+
+PatientAppointmentInstance.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    uniqueCode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      comment: "a unique code of the format A[SN][DD][MM][YY]",
+    },
+    patientName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    patientPhone: {
+      type: DataTypes.STRING,
+    },
+    patientAddress: {
+      type: DataTypes.STRING,
+    },
+    patientCity: {
+      type: DataTypes.STRING,
+    },
+    patientAge: {
+      type: DataTypes.INTEGER,
+    },
+    patientEmail: {
+      type: DataTypes.STRING,
+    },
+    patientSex: {
+      type: DataTypes.STRING,
+    },
+    appointmentStatus: {
+      type: DataTypes.STRING,
+    },
+    commentBefore: {
+      type: DataTypes.STRING,
+    },
+    commentAfter: {
+      type: DataTypes.STRING,
+    },
+    isFirstTime: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize: db,
+  }
+);
+
+export { PatientAppointmentInstance };
