f2aad14a5498e7fd3c56e6bc104aad80fed6568c
Elvis THOMME <elvisthomme@gmail.com>
2022-11-21 18:32:50 +0100

    Add checkPage to validate page param

--- dr-ng-api/src/validator/patientAppointmentValidator.ts
+++ dr-ng-api/src/validator/patientAppointmentValidator.ts
@@ -49,6 +49,74 @@ class PatientAppointmentValidator {
         .default(false),
     ];
   }
+  checkUpdatePatientAppointment() {
+    return [
+      body("id")
+        .notEmpty()
+        .withMessage("The patient id is required")
+        .isInt()
+        .withMessage("The patient id must be a number"),
+      body("patientName")
+        .optional()
+        .isString()
+        .withMessage("The patient name must be a string"),
+      body("patientAddress")
+        .optional()
+        .isString()
+        .withMessage("The patient address must be a string"),
+      body("patientCity")
+        .optional()
+        .isString()
+        .withMessage("The patient city must be a string"),
+      body("patientAge")
+        .optional()
+        .isNumeric()
+        .withMessage("The patient age must be a string"),
+      body("patientEmail")
+        .optional()
+        .isEmail()
+        .withMessage("The patient email must be a valid email"),
+      body("patientSex")
+        .optional()
+        .isIn(["male", "female"])
+        .withMessage("The patient sex must be male or female"),
+      body("appointmentStatus")
+        .optional()
+        .isIn(["passed", "missed", "rescheduled", "pending"])
+        .withMessage(
+          "The appointment status must be in ['passed', 'missed', 'rescheduled', 'pending']"
+        ),
+      body("commentBefore")
+        .optional()
+        .isNumeric()
+        .withMessage("The comment before must be a string"),
+      body("commentAfter")
+        .optional()
+        .isNumeric()
+        .withMessage("The comment after must be a string"),
+      body("isFirstTime")
+        .optional()
+        .isBoolean()
+        .withMessage("Is first time must be true of false"),
+    ];
+  }
+  checkDeleteAndGetById() {
+    return [
+      body("id")
+        .notEmpty()
+        .withMessage("The patient id is required")
+        .isInt()
+        .withMessage("The patient id must be a number"),
+    ];
+  }
+  checkPage() {
+    return [
+      body("page")
+        .optional()
+        .isInt()
+        .withMessage("The page must be a number"),
+    ];
+  }
 }
 
 export default new PatientAppointmentValidator();
